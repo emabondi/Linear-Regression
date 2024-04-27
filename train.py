5e30f1108e6c74418d186ee96339f76ecadd66cb
emabondi <emabond69@gmail.com>
2024-04-27 17:03:30 +0200

    makes more sense now... denormalized result theta

--- train.py
+++ train.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
+from matplotlib import pyplot as plt
 from predict import estimatePrice
+
+
+def plot(x, y, theta0, theta1):
+	plt.plot(x, y, 'ro', markersize=4)
+	plt.plot(x, theta0 + theta1 * x, 'b')
+	plt.xlabel('Mileage')
+	plt.ylabel('Price')
+	plt.show()
+
 def normalize(x):
 	return (x - x.min()) / (x.max() - x.min())
 
@@ -8,7 +18,7 @@ def train():
 	theta0 = 0.0
 	theta1 = 0.0
 	learningRate = 0.01
-	epochs = 100000
+	epochs = 10000
 	df = pd.read_csv('data.csv').astype(float)
 	x, y = df.iloc[:, 0].values, df.iloc[:, 1].values
 
@@ -27,6 +37,13 @@ def train():
 		
 		theta0 -= (learningRate / m) * tmp0
 		theta1 -= (learningRate / m) * tmp1
+
+	plot(x_norm, y_norm, theta0, theta1)
+
+	theta0 = theta0 * (y.max() - y.min()) + y.min() - theta1 * x.min() * (y.max() - y.min()) / (x.max() - x.min())
+	theta1 = theta1 * (y.max() - y.min()) / (x.max() - x.min())
+	
+	plot(x, y, theta0, theta1)
 
 	print(f'theta0:{theta0}  theta1:{theta1}' )
 	with open('thetas.csv', 'w') as f:
